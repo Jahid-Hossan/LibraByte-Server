d0fce9fa99b09554d9671ca725740a2fe7d36d64
isha35-1215 <tnishat652@gmail.com>
2024-02-15 19:48:17 +0600

    new arrivals issue fixed

--- src/routes/newArrivals/getNewArrivals/index.js
+++ src/routes/newArrivals/getNewArrivals/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const getNewArrivals = require('../../../apis/newArrivals/getNewArrivals');
-router.get('/newBooks', getNewArrivals);
+router.get('/books', getNewArrivals);
 module.exports = router;
 
