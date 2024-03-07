8b06193ae0fa87beb6d1ac72a0da28b16cf60169
isalim079 <salimreza1025@gmail.com>
2024-03-07 18:23:50 +0600

    complain server edited

--- src/routes/complains/getComplains/index.js
+++ src/routes/complains/getComplains/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const getComplains = require('../../../apis/complains/postComplains/postComplains');
-router.get('/complain/v1', getComplains);
+const getComplains = require('../../../apis/complains/getComplains/getComplains');
+router.get('/complains/v1', getComplains);
 module.exports = router;
 
